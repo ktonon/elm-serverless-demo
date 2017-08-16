a63cb26be90c89cce0537a1e11806cd5262dd490
Frank Schmid <fschmid@stashimi.com>
2017-08-16 11:14:34 +0200

    Use Serverless-webpack V3 RC.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
+const slsw = require('serverless-webpack');
 
 const config = {
-  entry: {
-    config: './src/Config/api.js',
-    forms: './src/Forms/api.js',
-    hello: './src/Hello/api.js',
-    interop: './src/Interop/api.js',
-    pipelines: './src/Pipelines/api.js',
-    quoted: './src/Quoted/api.js',
-    routing: './src/Routing/api.js',
-    sideEffects: './src/SideEffects/api.js',
-  },
+  entry: slsw.lib.entries,
   target: 'node', // Ignores built-in modules like path, fs, etc.
 
   output: {
@@ -21,23 +13,33 @@ const config = {
   },
 
   module: {
-    loaders: [{
-      // Compiles elm to JavaScript.
-      test: /\.elm$/,
-      exclude: [/elm-stuff/, /node_modules/],
-      loader: 'elm-webpack-loader',
-    }],
+    rules: [
+      {
+        // Compiles elm to JavaScript.
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: [
+          {
+            loader: 'elm-webpack-loader'
+          }
+        ]
+      }
+    ]
   },
 };
 
 if (process.env.NODE_ENV === 'production') {
   // Bridge is written for node 6.10. While AWS Lambda supports it
   // the UglifyJsPlugin does not :( so until that happens we use babel.
-  config.module.loaders.push({
+  config.module.rules.push({
     test: /\.js$/,
     exclude: [/elm-stuff/, /node_modules/],
-    loader: 'babel-loader',
-    options: { presets: 'env' },
+    use: [
+      {
+        loader: 'babel-loader',
+        options: { presets: 'env' }
+      }
+    ]
   });
 
   config.plugins = config.plugins || [];
