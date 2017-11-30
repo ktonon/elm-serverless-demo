4350f13122546386085530d8079c8efac829094f
Kevin Tonon <nobigb@gmail.com>
2017-11-30 08:36:36 -0500

    Removing babel loader. Using ES6 compatible uglifier. Updating serverless-webpack to 4.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const slsw = require('serverless-webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
   entry: slsw.lib.entries,
@@ -29,21 +30,8 @@ const config = {
 };
 
 if (process.env.NODE_ENV === 'production') {
-  // Bridge is written for node 6.10. While AWS Lambda supports it
-  // the UglifyJsPlugin does not :( so until that happens we use babel.
-  config.module.rules.push({
-    test: /\.js$/,
-    exclude: [/elm-stuff/, /node_modules/],
-    use: [
-      {
-        loader: 'babel-loader',
-        options: { presets: 'env' }
-      }
-    ]
-  });
-
   config.plugins = config.plugins || [];
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+  config.plugins.push(new UglifyJsPlugin());
 }
 
 module.exports = config;
